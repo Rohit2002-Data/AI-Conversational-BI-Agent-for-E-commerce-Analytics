b06111622a906de43ced557df381d1c76f5d77fe
Rohit2002-Data <rohitkumargitam021@gmail.com>
2026-03-28 13:04:56 +0530

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -1,13 +1,12 @@
 import duckdb
+import streamlit as st
 
+@st.cache_resource
 def load_database():
     con = duckdb.connect("ecommerce.db")
 
-    con.execute("CREATE TABLE IF NOT EXISTS orders AS SELECT * FROM 'data/orders.csv'")
-    con.execute("CREATE TABLE IF NOT EXISTS order_products_prior AS SELECT * FROM 'data/order_products__prior.csv'")
-    con.execute("CREATE TABLE IF NOT EXISTS order_products_train AS SELECT * FROM 'data/order_products__train.csv'")
-    con.execute("CREATE TABLE IF NOT EXISTS products AS SELECT * FROM 'data/products.csv'")
-    con.execute("CREATE TABLE IF NOT EXISTS aisles AS SELECT * FROM 'data/aisles.csv'")
-    con.execute("CREATE TABLE IF NOT EXISTS departments AS SELECT * FROM 'data/departments.csv'")
+    con.execute("CREATE OR REPLACE TABLE orders AS SELECT * FROM 'data/orders.csv'")
+    con.execute("CREATE OR REPLACE TABLE order_products_prior AS SELECT * FROM 'data/order_products__prior.csv'")
+    con.execute("CREATE OR REPLACE TABLE products AS SELECT * FROM 'data/products.csv'")
 
     return con
